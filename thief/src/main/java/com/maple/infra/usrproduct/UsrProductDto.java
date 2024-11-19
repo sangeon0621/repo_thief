ce58b4f60254ccbeee46130525494021974b9b47
sangeon0621 <tkddjs9604@gmail.com>
2024-11-19 14:30:41 +0900

    thief

--- thief/src/main/java/com/maple/infra/usrproduct/UsrProductDto.java
+++ thief/src/main/java/com/maple/infra/usrproduct/UsrProductDto.java
@@ -89,6 +89,10 @@ public class UsrProductDto {
 	private Date plDatetimeSvr;
 	private String member_seq;
 	
+	private String tid; // 결제 고유 번호
+    private String next_redirect_mobile_url; // 모바일 웹일 경우 받는 결제페이지 url
+    private String next_redirect_pc_url; // pc 웹일 경우 받는 결제 페이지
+    private String created_at;
 	
 //	-----
 	public String getPddSeq() {
@@ -558,6 +562,30 @@ public class UsrProductDto {
 	}
 	public void setMember_seq(String member_seq) {
 		this.member_seq = member_seq;
+	}
+	public String getTid() {
+		return tid;
+	}
+	public void setTid(String tid) {
+		this.tid = tid;
+	}
+	public String getNext_redirect_mobile_url() {
+		return next_redirect_mobile_url;
+	}
+	public void setNext_redirect_mobile_url(String next_redirect_mobile_url) {
+		this.next_redirect_mobile_url = next_redirect_mobile_url;
+	}
+	public String getNext_redirect_pc_url() {
+		return next_redirect_pc_url;
+	}
+	public void setNext_redirect_pc_url(String next_redirect_pc_url) {
+		this.next_redirect_pc_url = next_redirect_pc_url;
+	}
+	public String getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
 	}
 	
 	
